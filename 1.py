1eb8f78bc88d4549e6212a528dce6be314de519a
AidarbekNurali <agybay2007@gmail.com>
2024-05-27 18:49:59 +0600

    first commit

---
+++ 1.py
@@ -0,0 +1,29 @@
+
+a=0
+while True:
+    if a == 3:
+        break
+    print("Hi")
+    a+=1
+
+
+# set1={1,2,3,4,5}
+# b= set (1,2,3,4)
+    
+# a=(1,2,3)
+# b=turpe(1,2,3,4)
+#
+
+a={"name":"Nurali"}
+print(type(a))
+if type(a)==dict:
+    print("true")
+else:
+    print("false")
+
+
+a=4
+while a<5:
+    print("hello world")
+    a+=1
+        
